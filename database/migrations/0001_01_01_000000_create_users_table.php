5e68e661f15a568ee338e98803a41868c86619ec
rivalramadhan <ripallez@gmail.com>
2025-07-20 17:42:26 +0700

    refactor: update users table structure and add new migration files for facilities and complaints

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama');
+            $table->string('nik')->unique();;
             $table->string('password');
-            $table->rememberToken();
+            $table->string('no_telp')->unique()->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('role')->default('warga');
             $table->timestamps();
         });
 
---
+++ database/migrations/2025_07_20_101910_create_table_pengaduan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_pengaduan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('fasilitas_id')->constrained('table_fasilitas')->onDelete('cascade');
+            $table->text('judul_pengaduan')->nullable();
+            $table->text('isi_pengaduan');
+            $table->date('tanggal_pengaduan')->nullable();
+            $table->string('file_pengaduan');
+            $table->string('status')->default('pending');
+            $table->string('format_file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_pengaduan');
+    }
+};
