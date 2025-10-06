3349799758adbb23c80baae33fad25aed7600914
rivalramadhan <ripallez@gmail.com>
2025-10-07 05:25:35 +0700

    refactor: clean up comments in UserSeeder for clarity

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,17 +17,17 @@ class UserSeeder extends Seeder
         // Contoh membuat user dengan role 'warga'
         User::create([
             'nama' => 'Warga Tirtomoyo',
-            'nik' => '1234567890123456', // Pastikan 16 digit
-            'password' => Hash::make('password'), // Password akan di-hash
+            'nik' => '1234567890123456', 
+            'password' => Hash::make('password'),
             'no_telp' => '081234567890',
             'alamat' => 'Jl. Mawar No. 1, Desa Tirtomoyo',
-            'role' => 'warga', // Role default, tapi baik untuk ditulis eksplisit
+            'role' => 'warga',
         ]);
 
         // Contoh membuat user dengan role 'admin'
         User::create([
             'nama' => 'Admin Desa',
-            'nik' => '9876543210987654', // Harus unik
+            'nik' => '9876543210987654',
             'password' => Hash::make('adminpassword'),
             'no_telp' => '089876543210',
             'alamat' => 'Kantor Desa Tirtomoyo',
