f1bf4d2ccdfe27607adc9b075e0be752b8258245
rivalramadhan <ripallez@gmail.com>
2025-10-06 22:45:02 +0700

    feat: add Pengaduan model and migration for managing complaints

---
+++ database/migrations/2025_10_06_154156_create_pengaduans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengaduans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('fasilitas_id')->constrained('fasilitas')->onDelete('cascade');
+            $table->string('judul');
+            $table->text('isi');
+            $table->string('lokasi');
+            $table->date('tanggal_kejadian');
+            $table->string('subjek');
+            $table->string('lampiran');
+            $table->enum('status', ['dikirim', 'diproses', 'selesai'])->default('dikirim');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengaduans');
+    }
+};
